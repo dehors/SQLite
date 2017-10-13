0633e557a5243953ff69c054414cde7155465b8c
slam24 <luismartinez242011@gmail.com>
2017-10-13 05:58:02 -0600

    feat: Create register

--- app/src/main/java/com/example/slam24/sqlite/signup.java
+++ app/src/main/java/com/example/slam24/sqlite/signup.java
@@ -1,14 +1,21 @@
 package com.example.slam24.sqlite;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import OpenHelper.SQLite_OpenHelper;
 
 public class signup extends AppCompatActivity {
-
+    Button btnSaveUsu;
+    EditText txtNameUsu, txtDisUsui, txtEmailUsu, txtPassUsu;
+    SQLite_OpenHelper helper = new SQLite_OpenHelper(this, "BD1",null,1);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +23,20 @@ public class signup extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btnSaveUsu = (Button)findViewById(R.id.buttonSigUp);
+        txtNameUsu = (EditText)findViewById(R.id.editTextSUName);
+        txtDisUsui = (EditText)findViewById(R.id.editTextSUDistritic);
+        txtEmailUsu = (EditText)findViewById(R.id.editTextSUEmail);
+        txtPassUsu = (EditText)findViewById(R.id.editTextSUPass);
+
+        btnSaveUsu.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                helper.open();
+                helper.insertRow(String.valueOf(txtNameUsu.getText()), String.valueOf(txtDisUsui.getText()),String.valueOf(txtEmailUsu.getText()),String.valueOf(txtPassUsu.getText()));
+                helper.close_();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
     }
